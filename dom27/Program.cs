5afba7da3b4ac7907486b18feb6416ce2268c2d0
Sidorova Tania <sidorovatania3@gmail.com>
2022-11-26 13:23:01 +0300

    Добавлена задача 27

---
+++ dom27/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Enter number N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int numberN)
+{
+
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = numberN - numberN % 10;
+        result = result + (numberN - advance);
+        numberN = numberN / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Total sum: " + sumNumber);
